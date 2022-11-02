e967ec60ebfed4e7667bcf476b2ba9f80248f70e
aarti yadav <aarti.yadav@audacioustechnologies.com>
2022-11-02 11:56:27 +0530

    all module deploye

---
+++ src/Pages/CalenderView/LeaveList/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ListView from "../ListView";
+import styles from "./styles.module.less";
+interface permissionData {
+  leavePermission?: string;
+  searched?: any;
+  PropFromDate?: any;
+  PropToDate?: any;
+}
+
+const LeaveList: React.FC<permissionData> = (
+  leavePermission: permissionData,
+  { searched, PropFromDate, PropToDate }: permissionData
+) => {
+  console.log(searched,"filler")
+  return (
+    <div className={styles.listTable}>
+      <ListView
+        leavePermission="admin"
+        searched={leavePermission.searched}
+        PropFromDate={leavePermission.PropFromDate}
+        PropToDate={leavePermission.PropToDate}
+      />
+    </div>
+  );
+};
+export default LeaveList;
